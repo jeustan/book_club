88180e76c9c3e362e7e6e1da57713d045ab6b47f
Justin Reed <j.reed715@gmail.com>
2025-08-25 23:24:43 -0400

    sizing

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,6 @@
 const Book = ( { title }) => {
     return (
-        <img src={`https://golden-empanada-1c708f.netlify.app/assets/books/${title}.jpg`} className="aspect-2/3 h-70 object-cover" />
+        <img src={`https://golden-empanada-1c708f.netlify.app/assets/books/${title}.jpg`} className="aspect-2/3 h-40 object-cover" />
     )
 }
 
