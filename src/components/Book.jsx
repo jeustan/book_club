5eb83397f8c8d3a64c106e891ee88516d0d86a69
Justin Reed <j.reed715@gmail.com>
2025-08-25 22:53:03 -0400

    assets

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,7 @@
 const Book = ( { title }) => {
     return (
        
-            <img src={`/public/books/${title}.jpg`} className="aspect-2/3 h-70 object-cover" />
+            <img src={`/public/assets/books/${title}.jpg`} className="aspect-2/3 h-70 object-cover" />
         
     )
 }
