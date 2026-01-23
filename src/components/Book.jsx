f7413b510712d32ddd1d2aa416221a023074faa7
Justin Reed <j.reed715@gmail.com>
2026-01-23 12:02:22 -0500

    book element project name

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,7 @@
 const Book = ( { title }) => {
     return (
         <div>
-            <img src={`https://golden-empanada-1c708f.netlify.app/assets/books/${title}.jpg`} className="aspect-2/3 h-50 object-cover" />
+            <img src={`https://leah-justin-bookclub.netlify.app/assets/books/${title}.jpg`} className="aspect-2/3 h-50 object-cover" />
         </div>
     )
 }
